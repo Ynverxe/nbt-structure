65cfcfb3595ba579aba51b74293f2823c13ac1d3
Ynverxe <nahubar65@gmail.com>
2023-02-03 16:54:51 -0300

    feat(nbt-compound): implement java.util.Map interface

--- src/main/java/com/github/ynverxe/nbt_structure/nbt/NBTCompound.java
+++ src/main/java/com/github/ynverxe/nbt_structure/nbt/NBTCompound.java
@@ -9,10 +9,11 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @SuppressWarnings({"rawtypes", "unchecked", "UnusedReturnValue", "unused"})
-public class NBTCompound extends NBT<Map<String, NBT>> implements Cloneable, TreeNBTContainer {
+public class NBTCompound extends NBT<Map<String, NBT>>
+        implements Map<String, NBT>, TreeNBTContainer, Cloneable {
 
     public NBTCompound() {
-        this(new ConcurrentHashMap<>());
+        this(Collections.emptyMap());
     }
 
     protected NBTCompound(Map<String, NBT> value) {
@@ -65,5 +66,69 @@ public class NBTCompound extends NBT<Map<String, NBT>> implements Cloneable, Tre
     @Override
     public TreeNBTContainer createNewBranch() {
         return new NBTCompound();
+    }
+
+    @Override
+    public int size() {
+        return value.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return value.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return value.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return this.value.containsValue(value);
+    }
+
+    @Override
+    public NBT get(Object key) {
+        return value.get(key);
+    }
+
+    @Nullable
+    @Override
+    public NBT put(String key, NBT value) {
+        return this.value.put(key, value);
+    }
+
+    @Override
+    public NBT remove(Object key) {
+        return value.remove(key);
+    }
+
+    @Override
+    public void putAll(@NotNull Map<? extends String, ? extends NBT> m) {
+        value.putAll(m);
+    }
+
+    @Override
+    public void clear() {
+        value.clear();
+    }
+
+    @NotNull
+    @Override
+    public Set<String> keySet() {
+        return value.keySet();
+    }
+
+    @NotNull
+    @Override
+    public Collection<NBT> values() {
+        return value.values();
+    }
+
+    @NotNull
+    @Override
+    public Set<Entry<String, NBT>> entrySet() {
+        return value.entrySet();
     }
 }
