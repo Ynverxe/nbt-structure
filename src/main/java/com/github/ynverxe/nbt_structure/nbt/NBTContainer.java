b8ff1b5e9ff7ba0b56e9bbbbfb5db230b21d81f9
Ynverxe <nahubar65@gmail.com>
2023-02-03 17:20:29 -0300

    feat(nbt-container): add utility method

--- src/main/java/com/github/ynverxe/nbt_structure/nbt/NBTContainer.java
+++ src/main/java/com/github/ynverxe/nbt_structure/nbt/NBTContainer.java
@@ -4,10 +4,19 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
 import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 
 public interface NBTContainer extends NBTWritable, NBTReadable {
 
     @NotNull Set<String> keys();
+
+    default <T extends NBT<?>> @NotNull T writeIfAbsent(@NotNull String key, @NotNull Supplier<T> nbtSupplier) {
+        T found = read(key);
+
+        if (found == null) write(key, found = nbtSupplier.get());
+
+        return found;
+    }
 
     default @NotNull NBTCompound toCompound() {
         NBTCompound compound = new NBTCompound();
